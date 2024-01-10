0467d62f3cf8308eb66e8233e3e3cfd6cf1531e4
Antti Kivi <antti.kivi@visiosto.fi>
2024-01-10 21:22:26 +0200

    feat: disable unused UI plugins

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    enabled = false,
+  },
+  {
+    "stevearc/dressing.nvim",
+    enabled = false,
+  },
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  {
+    "MunifTanjim/nui.nvim",
+    enabled = false,
+  },
+  {
+    "rcarriga/nvim-notify",
+    enabled = false,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    enabled = false,
+  },
+}
