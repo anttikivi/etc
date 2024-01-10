efe2569065c354e439a27c55d736420357666090
Antti Kivi <antti.kivi@visiosto.fi>
2024-01-10 22:12:15 +0200

    feat: add the Copilot extra

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,11 +13,11 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup {
   spec = {
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+    },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
   defaults = {
