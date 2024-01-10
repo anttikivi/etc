5fdb32b605256a8b1d0367b83394f195c7a0c287
Antti Kivi <antti.kivi@visiosto.fi>
2024-01-10 22:07:36 +0200

    feat: configure Lualine

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,3 +1,5 @@
+local Util = require "lazyvim.util"
+
 return {
   {
     "akinsho/bufferline.nvim",
@@ -10,6 +12,36 @@ return {
   {
     "stevearc/dressing.nvim",
     enabled = false,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      return {
+        options = {
+          icons_enabled = false,
+          theme = "brunch",
+          component_separators = "|",
+          section_separators = "",
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = {
+            "branch",
+            { "diff",        colored = false },
+            { "diagnostics", colored = false },
+          },
+          lualine_c = {
+            Util.lualine.root_dir(),
+            "filename",
+            { Util.lualine.pretty_path() },
+          },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      }
+    end,
   },
   {
     "folke/noice.nvim",
