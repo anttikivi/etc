c3bd79e2f4e3a1e531aafba32010c2bb598e25d6
Antti Kivi <antti.kivi@visiosto.fi>
2024-01-10 20:59:49 +0200

    feat: disable colour schemes and add Brunch

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "anttikivi/brunch.nvim",
+    dev = true,
+    lazy = true,
+  },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+}
