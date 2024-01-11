6e1ad86922f6f60f1da7b92539062ff84f4feb8d
Antti Kivi <antti.kivi@visiosto.fi>
2024-01-11 22:30:12 +0200

    feat: add icons for todo-comments

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -98,6 +98,20 @@ return {
     end,
   },
   {
+    "folke/todo-comments.nvim",
+    opts = {
+      keywords = {
+        FIX = { icon = "☣︎ " },
+        TODO = { icon = "✓ " },
+        HACK = { icon = "⽕ " },
+        WARN = { icon = "⚠ " },
+        PERF = { icon = "⚡︎ " },
+        NOTE = { icon = "♫ " },
+        TEST = { icon = "⏲ " },
+      },
+    },
+  },
+  {
     "RRethy/vim-illuminate",
     enabled = false,
   },
