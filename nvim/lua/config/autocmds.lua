b28dfa8acc9fd4a17a4d5feddd1ba400ac6e6134
Antti Kivi <antti.kivi@visiosto.fi>
2024-01-11 22:07:59 +0200

    refactor: add the JSON autocommand to a group

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,5 +1,12 @@
+---@param name string
+---@return number
+local function augroup(name)
+  return vim.api.nvim_create_augroup("anttikivi_" .. name, { clear = true })
+end
+
 -- Fix the concealing of JSON files
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup "fix_json_conceal",
   pattern = { "json", "jsonc" },
   callback = function()
     vim.wo.spell = false
