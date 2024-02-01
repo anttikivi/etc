71fb520e3931f183649c78d2c93b0fe44e7e2180
Antti Kivi <antti.kivi@visiosto.fi>
2024-02-01 15:39:26 +0200

    feat: add Tree-sitter parser for Go templates

--- nvim/lua/anttikivi/parsers.lua
+++ nvim/lua/anttikivi/parsers.lua
@@ -7,3 +7,13 @@ parser_config.blade = {
   },
   filetype = "blade",
 }
+
+parser_config.gotmpl = {
+  install_info = {
+    url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    files = { "src/parser.c" },
+  },
+  filetype = "gotmpl",
+  -- used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
+  used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" },
+}
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(
           opts.ensure_installed,
-          { "astro", "blade", "css", "php" }
+          { "astro", "blade", "css", "gotmpl", "php" }
         )
       end
     end,
