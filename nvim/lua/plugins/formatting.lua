a1f08a1e1718367160445dbc4579eda4f2d88b07
Antti Kivi <antti.kivi@visiosto.fi>
2024-02-19 21:21:56 +0200

    feat: add formatters for Blade templates and CSS files

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -3,6 +3,8 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        blade = { "prettier" },
+        css = { "prettier" },
         gohtmltmpl = { "prettier" },
         lua = { "stylua" },
         sh = { "shfmt" },
