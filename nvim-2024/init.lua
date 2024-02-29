1879cdc2f441bae007f4484be1d4dca313f7d9e6
Antti Kivi <antti.kivi@visiosto.fi>
2024-02-29 21:44:05 +0200

    feat: add the basic options

--- nvim-2024/init.lua
+++ nvim-2024/init.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+require "anttikivi.config.options"
+
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
