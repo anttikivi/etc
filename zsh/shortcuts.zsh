a17781459ecd5b02c21cc6a7bfdda0903211cb20
Antti Kivi <antti.kivi@visiosto.fi>
2024-03-02 13:14:41 +0200

    refactor: remove unused shortcuts

--- zsh/shortcuts.zsh
+++ zsh/shortcuts.zsh
@@ -1,17 +1,11 @@
-alpinejs_ls=$HOME/personal/alpinejs-language-server
-ansible_dir=$HOME/ansible
 aoc=$HOME/personal/advent-of-code
 brunch=$HOME/plugins/brunch.nvim
 dot=$HOME/dotfiles
-obsidian="$HOME/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents"
 visiosto=$HOME/visiosto
 website=$HOME/personal/website
 
-alias cdals="cd $alpinejs_ls"
-alias cdans="cd $ansible_dir"
 alias cdaoc="cd $aoc"
 alias cdb="cd $brunch"
 alias cdd="cd $dot"
-alias cdnote="cd $obsidian/Personal"
 alias cdv="cd $visiosto"
 alias cdweb="cd $website"
