213801a51dd97180ea652ae4cb48e5a0fcc681f5
Antti Kivi <antti.kivi@visiosto.fi>
2024-05-30 14:45:50 +0300

    feat(nvim): use relative paths with filenames on Lualine

--- nvim/lua/anttikivi/plugins/ui.lua
+++ nvim/lua/anttikivi/plugins/ui.lua
@@ -28,7 +28,12 @@ return {
             { "diff", colored = false },
             { "diagnostics", colored = false },
           },
-          lualine_c = { "filename" },
+          -- 0: Just the filename
+          -- 1: Relative path
+          -- 2: Absolute path
+          -- 3: Absolute path, with tilde as the home directory
+          -- 4: Filename and parent dir, with tilde as the home directory
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = { "encoding", "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
@@ -36,7 +41,7 @@ return {
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = { "filename" },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = { "location" },
           lualine_y = {},
           lualine_z = {},
