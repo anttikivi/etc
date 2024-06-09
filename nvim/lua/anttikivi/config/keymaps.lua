7b5a5ce3deca5c5606783c97bb417ddea1efb5e3
Antti Kivi <antti.kivi@visiosto.fi>
2024-06-09 14:40:59 +0300

    feat(nvim): remove second exclamation marks from the arrow movement warnings as that was a bit too much

--- nvim/lua/anttikivi/config/keymaps.lua
+++ nvim/lua/anttikivi/config/keymaps.lua
@@ -51,10 +51,10 @@ vim.keymap.set(
 )
 
 -- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!"<CR>')
 
 -- Keybinds to make split navigation easier.
 vim.keymap.set(
