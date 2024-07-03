7e0ac5f5d236fc5c7babf71b8266ec2bca879e44
Antti Kivi <antti.kivi@visiosto.fi>
2024-07-03 12:39:39 +0300

    feat(nvim): display preview of `.png` and `.jpg` images in Telescope

--- nvim/lua/anttikivi/plugins/editor.lua
+++ nvim/lua/anttikivi/plugins/editor.lua
@@ -116,6 +116,39 @@ return {
               ["<C-k>"] = require("telescope.actions").move_selection_previous,
             },
           },
+          preview = {
+            mime_hook = function(filepath, bufnr, opts)
+              local is_image = function(file)
+                local image_extensions = { "png", "jpg" }
+                local split_path =
+                  vim.split(file:lower(), ".", { plain = true })
+                local extension = split_path[#split_path]
+                return vim.tbl_contains(image_extensions, extension)
+              end
+              if is_image(filepath) then
+                local term = vim.api.nvim_open_term(bufnr, {})
+                local function send_output(_, data, _)
+                  for _, d in ipairs(data) do
+                    vim.api.nvim_chan_send(term, d .. "\r\n")
+                  end
+                end
+                vim.fn.jobstart({
+                  "catimg",
+                  filepath,
+                }, {
+                  on_stdout = send_output,
+                  stdout_buffered = true,
+                  pty = true,
+                })
+              else
+                require("telescope.previewers.utils").set_preview_message(
+                  bufnr,
+                  opts.winid,
+                  "Binary cannot be previewed"
+                )
+              end
+            end,
+          },
           vimgrep_arguments = vimgrep_arguments,
         },
         extensions = {
