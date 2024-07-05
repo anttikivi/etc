4355d8bb185a48a78e593b5921af8b3d5b692103
Antti Kivi <antti.kivi@visiosto.fi>
2024-07-05 14:12:28 +0300

    feat(zsh): use case-insensitive completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -2,3 +2,5 @@ unsetopt MENU_COMPLETE
 setopt ALWAYS_TO_END
 setopt AUTO_MENU
 setopt COMPLETE_IN_WORD
+
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|=*' 'l:|=* r:|=*'
