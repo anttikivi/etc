3790c822fc95e356de01346a1996ceede6668892
Antti Kivi <antti.kivi@visiosto.fi>
2024-07-05 11:24:31 +0300

    feat(zsh): add an alias for reloading `.zshrc` and clean up the file

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,4 @@
-# +--------------------------------+
-# | Navigation and directory stack |
-# +--------------------------------+
+alias reload="source ~/.zshrc"
 
 alias -- -="cd -"
 alias -g ...=../..
@@ -10,11 +8,12 @@ alias -g ......=../../../../..
 alias _="sudo "
 
 alias d="dirs -v"
-for index ({1..9}) alias "$index"="cd +${index} > /dev/null"; unset index # directory stack
 
-# +----+
-# | ls |
-# +----+
+# Directory stack
+for index in {1..9}; do
+  alias "$index"="cd +${index} > /dev/null"
+done
+unset index
 
 alias ls="ls --color=auto"
 alias l="ls -lh"
@@ -23,53 +22,25 @@ alias ll="ls -lAhF"
 alias lls="ls -lAhFtr"
 alias lc="ls -CF"
 
-# +-----------------+
-# | File operations |
-# +-----------------+
-
 alias cp="cp -i"
 alias mv="mv -i"
 alias rm="rm -i"
 alias md="mkdir -p"
 alias rd="rmdir"
 
-# +------+
-# | grep |
-# +------+
-
 alias grep="grep --color"
-
-# +-------+
-# | clear |
-# +-------+
 
 alias cls="tput reset"
 
-# +--------+
-# | whence |
-# +--------+
 # Make the aliases for the different command implementations explicit.
-
 alias which="whence -c"
 alias type="whence -v"
 alias where="whence -ca"
 
-# +----+
-# | du |
-# +----+
-
 alias duf="du -sh * | sort -hr"
-
-# +------+
-# | nvim |
-# +------+
 
 alias vi="nvim"
 alias vim="nvim"
-
-# +----------------+
-# | Pipe shortcuts |
-# +----------------+
 
 alias -g H="| head"
 alias -g T="| tail"
@@ -79,17 +50,9 @@ alias -g M="| most"
 alias -g LL="2>&1 | less"
 alias -g CA="2>&1 | cat -A"
 
-# +------+
-# | tmux |
-# +------+
-
 alias td="tmux detach"
 alias tks="tmux kill-server"
 alias tls="tmux list-sessions"
-
-# +-----------+
-# | Terraform |
-# +-----------+
 
 alias tf="terraform"
 alias terrafrom="terraform"
