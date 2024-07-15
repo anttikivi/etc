c0b469186840bdb80dc7868304638ec3d47d3d0d
Antti Kivi <antti.kivi@visiosto.fi>
2024-07-16 00:29:40 +0300

    feat: use Catppuccin in WezTerm

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,6 +1,21 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
-config.color_scheme = "AdventureTime"
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 return config
