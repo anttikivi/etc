039b2df69d506b9e6402fdec09d6d143fc958640
Antti Kivi <antti.kivi@visiosto.fi>
2024-07-16 11:25:55 +0300

    feat(wezterm): change the window padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,9 +5,18 @@ local keys = require "keys"
 
 local config = Config:init()
 
+---@type number
+local window_padding = 6
+
 config:append {
   automatically_reload_config = true,
   enable_tab_bar = false,
+  window_padding = {
+    left = window_padding,
+    right = window_padding,
+    top = window_padding,
+    bottom = window_padding,
+  },
 }
 
 config:append(colorscheme)
