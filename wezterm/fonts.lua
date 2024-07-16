c449aa4e64c5e3a9a8d960036b1fa3040ab6936f
Antti Kivi <antti.kivi@visiosto.fi>
2024-07-16 21:17:30 +0300

    feat(wezterm): add additional font styling options

--- wezterm/fonts.lua
+++ wezterm/fonts.lua
@@ -4,6 +4,14 @@ return {
   font = wezterm.font {
     family = "JetBrains Mono",
     weight = "Regular",
+    harfbuzz_features = { "calt=1", "clig=1", "liga=1", "zero=1" },
   },
-  font_size = 12.5,
+  -- The recommended settings for JetBrains Mono:
+  -- font_size: 13,
+  -- line_height: 1.2,
+  font_size = 13,
+  line_height = 1.1,
+  -- freetype_load_flags = "NO_HINTING",
+  -- freetype_load_target = "Light",
+  -- freetype_render_target = "HorizontalLcd",
 }
