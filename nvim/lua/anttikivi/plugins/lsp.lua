ac710cea37bb5f50ecd7ad233e1398341c84d4b6
Antti Kivi <antti.kivi@visiosto.fi>
2024-07-21 14:35:16 +0300

    feat(nvim): use `vtsls` instead of `tsserver`

--- nvim/lua/anttikivi/plugins/lsp.lua
+++ nvim/lua/anttikivi/plugins/lsp.lua
@@ -122,8 +122,60 @@ return {
           filetypes_include = {},
         },
         terraformls = {},
-        tsserver = {},
+        tsserver = {
+          enable = false,
+        },
         vimls = {},
+        vtsls = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+              tsserver = {
+                globalPlugins = {
+                  {
+                    name = "@astrojs/ts-plugin",
+                    location = util.get_pkg_path(
+                      "astro-language-server",
+                      "/node_modules/@astrojs/ts-plugin"
+                    ),
+                    enableForWorkspaceTypeScriptVersions = true,
+                  },
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+          -- TODO: See if it is worth it to implement the keymap overwrites for
+          -- vtsls.
+        },
         yamlls = {
           capabilities = {
             textDocument = {
--- nvim/lua/anttikivi/util/init.lua
+++ nvim/lua/anttikivi/util/init.lua
@@ -100,4 +100,35 @@ function M.set_global_variables()
     or "sunday"
 end
 
+--- Gets a path to a package in the Mason registry.
+---
+--- This function is originally from `LazyVim/LazyVim` which is lisenced under
+--- the Apache License 2.0. See the original code at
+--- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/util/init.lua
+---
+---@param pkg string
+---@param path? string
+---@param opts? { warn?: boolean }
+function M.get_package_path(pkg, path, opts)
+  pcall(require, "mason") -- make sure Mason is loaded. Will fail when generating docs
+  local root = vim.env.MASON or (vim.fn.stdpath "data" .. "/mason")
+  opts = opts or {}
+  opts.warn = opts.warn == nil and true or opts.warn
+  path = path or ""
+  local ret = root .. "/packages/" .. pkg .. "/" .. path
+  if
+    opts.warn
+    and not vim.loop.fs_stat(ret)
+    and not require("lazy.core.config").headless()
+  then
+    M.warn(
+      ("Mason package path not found for **%s**:\n- `%s`\nYou may need to force update the package."):format(
+        pkg,
+        path
+      )
+    )
+  end
+  return ret
+end
+
 return M
