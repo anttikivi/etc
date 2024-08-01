863461bf369972be58fd2ffb6c3fcd65aedf575c
Antti Kivi <antti@anttikivi.fi>
2024-08-01 20:58:31 +0300

    feat: use the SSH key from 1Password

--- main.tf
+++ main.tf
@@ -22,5 +22,5 @@ data "google_client_openid_userinfo" "me" {
 
 resource "google_os_login_ssh_public_key" "default" {
   user = data.google_client_openid_userinfo.me.email
-  key  = file(var.os_login_public_key)
+  key  = var.os_login_public_key
 }
