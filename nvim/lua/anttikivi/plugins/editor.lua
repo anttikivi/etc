8cd634964384b2ffe29c64d9caf14f584f4eb1eb
Antti Kivi <antti@anttikivi.fi>
2024-09-26 17:22:54 +0300

    feat: remove the image previews from Telescope

--- nvim/lua/anttikivi/plugins/editor.lua
+++ nvim/lua/anttikivi/plugins/editor.lua
@@ -38,14 +38,14 @@ return {
       "nvim-lua/plenary.nvim",
     },
     config = function()
-      local harpoon = require "harpoon"
-      harpoon:setup {}
+      local harpoon = require("harpoon")
+      harpoon:setup({})
     end,
     -- TODO: Update this when the branch is merged into master.
     branch = "harpoon2",
     event = "VeryLazy",
     keys = function()
-      local harpoon = require "harpoon"
+      local harpoon = require("harpoon")
       local keys = {
         {
           "<C-a>",
@@ -97,18 +97,17 @@ return {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
         cond = function()
-          return vim.fn.executable "make" == 1
+          return vim.fn.executable("make") == 1
         end,
       },
       { "nvim-telescope/telescope-ui-select.nvim" },
       { "nvim-tree/nvim-web-devicons", enabled = vim.g.icons_enabled },
     },
     config = function()
-      local telescope = require "telescope"
-      local telescope_config = require "telescope.config"
+      local telescope = require("telescope")
+      local telescope_config = require("telescope.config")
 
-      local vimgrep_arguments =
-        { unpack(telescope_config.values.vimgrep_arguments) }
+      local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
 
       -- Search in hidden directories.
       table.insert(vimgrep_arguments, "--hidden")
@@ -117,7 +116,7 @@ return {
       table.insert(vimgrep_arguments, "--glob")
       table.insert(vimgrep_arguments, "!**/.git/*")
 
-      telescope.setup {
+      telescope.setup({
         defaults = {
           mappings = {
             i = {
@@ -125,39 +124,6 @@ return {
               ["<C-j>"] = require("telescope.actions").move_selection_next,
               ["<C-k>"] = require("telescope.actions").move_selection_previous,
             },
-          },
-          preview = {
-            mime_hook = function(filepath, bufnr, opts)
-              local is_image = function(file)
-                local image_extensions = { "png", "jpg" }
-                local split_path =
-                  vim.split(file:lower(), ".", { plain = true })
-                local extension = split_path[#split_path]
-                return vim.tbl_contains(image_extensions, extension)
-              end
-              if is_image(filepath) then
-                local term = vim.api.nvim_open_term(bufnr, {})
-                local function send_output(_, data, _)
-                  for _, d in ipairs(data) do
-                    vim.api.nvim_chan_send(term, d .. "\r\n")
-                  end
-                end
-                vim.fn.jobstart({
-                  "catimg",
-                  filepath,
-                }, {
-                  on_stdout = send_output,
-                  stdout_buffered = true,
-                  pty = true,
-                })
-              else
-                require("telescope.previewers.utils").set_preview_message(
-                  bufnr,
-                  opts.winid,
-                  "Binary cannot be previewed"
-                )
-              end
-            end,
           },
           vimgrep_arguments = vimgrep_arguments,
         },
@@ -178,89 +144,37 @@ return {
             },
           },
         },
-      }
+      })
 
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "ui-select")
 
-      local builtin = require "telescope.builtin"
+      local builtin = require("telescope.builtin")
 
-      vim.keymap.set(
-        "n",
-        "<leader>sh",
-        builtin.help_tags,
-        { desc = "[S]earch [H]elp" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>sk",
-        builtin.keymaps,
-        { desc = "[S]earch [K]eymaps" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>sf",
-        builtin.find_files,
-        { desc = "[S]earch [F]iles" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>ss",
-        builtin.builtin,
-        { desc = "[S]earch [S]elect Telescope" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>sw",
-        builtin.grep_string,
-        { desc = "[S]earch current [W]ord" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>sg",
-        builtin.live_grep,
-        { desc = "[S]earch by [G]rep" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>sd",
-        builtin.diagnostics,
-        { desc = "[S]earch [D]iagnostics" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>sr",
-        builtin.resume,
-        { desc = "[S]earch [R]esume" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>s.",
-        builtin.oldfiles,
-        { desc = '[S]earch Recent Files ("." for repeat)' }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader><leader>",
-        builtin.buffers,
-        { desc = "[ ] Find existing buffers" }
-      )
+      vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+      vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+      vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+      vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
       vim.keymap.set("n", "<leader>/", function()
-        builtin.current_buffer_fuzzy_find(
-          require("telescope.themes").get_dropdown {
-            winblend = 10,
-            previewer = false,
-          }
-        )
+        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+          winblend = 10,
+          previewer = false,
+        }))
       end, { desc = "[/] Fuzzily search in current buffer" })
       vim.keymap.set("n", "<leader>s/", function()
-        builtin.live_grep {
+        builtin.live_grep({
           grep_open_files = true,
           prompt_title = "Live Grep in Open Files",
-        }
+        })
       end, { desc = "[S]earch [/] in Open Files" })
       vim.keymap.set("n", "<leader>sn", function()
-        builtin.find_files { cwd = vim.fn.stdpath "config" }
+        builtin.find_files({ cwd = vim.fn.stdpath("config") })
       end, { desc = "[S]earch [N]eovim files" })
     end,
   },
