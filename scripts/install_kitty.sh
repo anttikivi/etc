dae2f876f7e2edd7f8a03dff983e005cdb30de18
Antti Kivi <antti@anttikivi.fi>
2024-09-28 10:25:32 +0300

    refactor: remove the parameter from the kitty installation and use the actual variables instead

--- scripts/install_kitty.sh
+++ scripts/install_kitty.sh
@@ -9,13 +9,13 @@ wanted_version="$(curl -LsSH "X-GitHub-Api-Version: 2022-11-28" https://api.gith
 current_version="$(kitty --version | cut -c $(("$(kitty --version | grep -ob "\ " | head -1 | grep -oE "[0-9]+")" + 2))-"$(kitty --version | grep -ob "\ " | head -2 | tail -1 | grep -oE "[0-9]+")")"
 
 install_kitty() {
-  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n installer="version-$(echo "$1" | cut -c 2-)"
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n installer="version-$(echo "${wanted_version}" | cut -c 2-)"
 }
 
 if ! command -v kitty >/dev/null 2>&1; then
-  install_kitty "${wanted_version}"
+  install_kitty
 elif [ "$(echo "${wanted_version}" | cut -c 2-)" != "${current_version}" ]; then
-  install_kitty "${wanted_version}"
+  install_kitty
 fi
 
 readonly CONFIG_DIR="${HOME}/.config/kitty"
