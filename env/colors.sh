15428ec738596e423015ed234771b716a010886c
Antti Kivi <antti@anttikivi.fi>
2024-09-28 13:22:03 +0300

    refactor: change Neovim to use the color environment variables instead of the custom file

--- env/colors.sh
+++ env/colors.sh
@@ -1,3 +1,4 @@
+# shellcheck disable=SC2034
 readonly CATPPUCCIN_NAME="catppuccin"
 
 readonly CATPPUCCIN_FRAPPE="frappe"
@@ -11,5 +12,7 @@ readonly ROSE_PINE_MOON="moon"
 readonly ROSE_PINE_DAWN="dawn"
 
 export COLOR_SCHEME="${ROSE_PINE_NAME}"
+export COLOR_SCHEME_DARK_VARIANT="${ROSE_PINE_MAIN}"
+export COLOR_SCHEME_LIGHT_VARIANT="${ROSE_PINE_DAWN}"
 export DARK_COLOR_SCHEME="${ROSE_PINE_NAME}"
 export LIGHT_COLOR_SCHEME="${ROSE_PINE_NAME}-${ROSE_PINE_DAWN}"
