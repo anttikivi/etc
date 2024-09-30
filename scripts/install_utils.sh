3b53175207c0d1a793839ee9dad934c69855cb64
Antti Kivi <antti@anttikivi.fi>
2024-09-30 22:13:19 +0300

    feat: add Rust installation and move some tools to be platform independent

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -10,7 +10,7 @@ not_supported() {
 os_name="$(uname)"
 
 if [ "${os_name}" = "Darwin" ]; then
-  echo "Running on Darwin"
+  echo "Running the Darwin-based installations"
 
   echo "Installing general utilities"
 
@@ -24,7 +24,7 @@ if [ "${os_name}" = "Darwin" ]; then
 
   echo "Installing tools"
 
-  pipx install ansible-lint clang-format
+  brew install clang-format
 
   if ! command -v aws >/dev/null 2>&1; then
     tmp_dir="$(mktemp -d "aws_cli")"
@@ -34,14 +34,6 @@ if [ "${os_name}" = "Darwin" ]; then
     ln -s ~/.local/opt/aws-cli/aws ~/.local/bin/aws
     ln -s ~/.local/opt/aws-cli/aws_completer ~/.local/bin/aws_completer
     rm -r "${tmp_dir}"
-
-    aws_config="${HOME}/.aws/config"
-
-    if [ -f "${aws_config}" ]; then
-      rm "${aws_config}"
-    fi
-
-    ansible-vault view ./templates/aws_config >"${aws_config}"
   fi
 
   if ! command -v gcloud >/dev/null 2>&1; then
@@ -66,3 +58,17 @@ elif [ "${os_name}" = "Linux" ]; then
 else
   not_supported "${os_name}"
 fi
+
+echo "Running the platform-independent utility installations"
+
+echo "Running the platform-independent tool installations"
+pipx install ansible-lint
+
+echo "Updating the AWS configuration"
+aws_config="${HOME}/.aws/config"
+
+if [ -f "${aws_config}" ]; then
+  rm "${aws_config}"
+fi
+
+ansible-vault view ./templates/aws_config >"${aws_config}"
--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,3 +3,4 @@
 export GO_VERSION="1.22.7"
 export KITTY_VERSION="v0.36.3"
 export NVIM_VERSION="v0.10.0"
+export TAPLO_VERSION="0.9.3"
