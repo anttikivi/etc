b74e53972bc63fde9a441b9864e875edc4614009
Antti Kivi <antti@anttikivi.fi>
2024-10-02 08:21:58 +0300

    feat: add the required line when needed to the kitty configuration and add an environment variable to control when the updates will be run

--- scripts/install_kitty.sh
+++ scripts/install_kitty.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+readonly ESC="\033"
+readonly ESC_RESET="${ESC}[0m"
+readonly ESC_YELLOW="${ESC}[33m"
+
 . ./scripts/versions.sh
 
 required_minor_version="$(echo "${KITTY_VERSION}" | head -c "$(echo "${KITTY_VERSION}" | grep -m 2 -ob "\." | tail -1 | grep -oE "[0-9]+")")"
@@ -9,27 +13,35 @@ wanted_version="$(curl -LsSH "X-GitHub-Api-Version: 2022-11-28" https://api.gith
 current_version="$(kitty --version | cut -c $(($(kitty --version | grep -ob "\ " | head -1 | grep -oE "[0-9]+") + 2))-"$(kitty --version | grep -ob "\ " | head -2 | tail -1 | grep -oE "[0-9]+")")"
 
 install_kitty() {
+  echo "kitty installation will be run"
   curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n installer="version-$(echo "${wanted_version}" | cut -c 2-)"
 }
 
 if ! command -v kitty >/dev/null 2>&1; then
   install_kitty
 elif [ "$(echo "${wanted_version}" | cut -c 2-)" != "${current_version}" ]; then
-  install_kitty
+  if [ "${DOTFILES_UPDATE}" = 1 ]; then
+    install_kitty
+  else
+    printf "%bkitty update available! Current version: %s, available version: %s%b" "${ESC_YELLOW}" "${current_version}" "${wanted_version}" "${ESC_RESET}"
+  fi
 fi
 
 readonly CONFIG_DIR="${HOME}/.config/kitty"
 readonly CONFIG_FILE="${CONFIG_DIR}/kitty.conf"
+readonly CONFIG_LINE="include user.conf"
 
 if [ ! -d "${CONFIG_DIR}" ]; then
   mkdir -p "${CONFIG_DIR}"
 fi
 
-if [ -e "${CONFIG_FILE}" ]; then
-  rm "${CONFIG_FILE}"
+if [ ! -e "${CONFIG_FILE}" ]; then
+  touch "${CONFIG_FILE}"
 fi
 
-cp ./templates/kitty.conf "${CONFIG_FILE}"
+if ! grep -xF "${CONFIG_LINE}" "${CONFIG_FILE}" >/dev/null 2>&1; then
+  echo "${CONFIG_LINE}" >>"${CONFIG_FILE}"
+fi
 
 os_name="$(uname)"
 
@@ -37,6 +49,7 @@ if [ "${os_name}" = "Darwin" ]; then
   echo "Creating the Darwin-specific kitty binary links"
   ln -sf /Applications/kitty.app/Contents/MacOS/kitty /Applications/kitty.app/Contents/MacOS/kitten ~/.local/bin/
 
+  # TODO: Maybe modify this so that the daemon is not reloaded every time.
   echo "Creating and starting the Darwin-specific kitty daemon"
   launch_agent="fi.anttikivi.kittycolors.plist"
 
