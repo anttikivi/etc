fff971b074e2da8e6b154b71e188f3187abdb6a5
Antti Kivi <antti@anttikivi.fi>
2024-10-07 16:07:41 +0300

    fix: check for the kitty updates only if the script has an Internet connection

--- scripts/kitty.sh
+++ scripts/kitty.sh
@@ -1,43 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-. ./utils/colors.sh
-. ./versions.sh
+source ./utils/colors.sh
+source ./versions.sh
 
-required_minor_version="$(echo "${KITTY_VERSION}" | head -c "$(echo "${KITTY_VERSION}" | grep -m 2 -ob "\." | tail -1 | grep -oE "[0-9]+")")"
-wanted_version="$(curl -LsSH "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/kovidgoyal/kitty/releases | jq -r '.[] | .tag_name' | grep "${required_minor_version}" | sort -V | tail -1)"
-current_version="$(kitty --version | cut -c $(($(kitty --version | grep -ob "\ " | head -1 | grep -oE "[0-9]+") + 2))-"$(kitty --version | grep -ob "\ " | head -2 | tail -1 | grep -oE "[0-9]+")")"
+if [ "${HAS_CONNECTION}" = "true" ]; then
+  minor_ver="$(echo "${KITTY_VERSION}" | head -c "$(echo "${KITTY_VERSION}" | grep -m 2 -ob "\." | tail -1 | grep -oE "[0-9]+")")"
+  wanted_ver="$(curl -LsSH "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/kovidgoyal/kitty/releases | jq -r '.[] | .tag_name' | grep "${minor_ver}" | sort -V | tail -1)"
+  current_ver="$(kitty --version | cut -c $(($(kitty --version | grep -ob "\ " | head -1 | grep -oE "[0-9]+") + 2))-"$(kitty --version | grep -ob "\ " | head -2 | tail -1 | grep -oE "[0-9]+")")"
 
-install_kitty() {
-  echo "kitty installation will be run"
-  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n installer="version-$(echo "${wanted_version}" | cut -c 2-)"
-}
+  install_kitty() {
+    echo "Running kitty installation"
+    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n installer="version-$(echo "${wanted_ver}" | cut -c 2-)"
+  }
 
-if ! command -v kitty >/dev/null 2>&1; then
-  install_kitty
-elif [ "$(echo "${wanted_version}" | cut -c 2-)" != "${current_version}" ]; then
-  if [ "${DOTFILES_UPDATE}" = 1 ]; then
+  if ! command -v kitty >/dev/null 2>&1; then
     install_kitty
-  else
-    printf "%bkitty update available! Current version: %s, available version: %s%b" "${DOTFILES_ESC_YELLOW}" "${current_version}" "${wanted_version}" "${DOTFILES_ESC_RESET}"
+  elif [ "$(echo "${wanted_ver}" | cut -c 2-)" != "${current_ver}" ]; then
+    if [ "${DOTFILES_UPDATE}" = 1 ]; then
+      install_kitty
+    else
+      printf "%bkitty update available! Current version: %s, available version: %s%b" "${DOTFILES_ESC_YELLOW}" "${current_ver}" "${wanted_ver}" "${DOTFILES_ESC_RESET}"
+    fi
   fi
 fi
 
-readonly CONFIG_DIR="${HOME}/.config/kitty"
-readonly CONFIG_FILE="${CONFIG_DIR}/kitty.conf"
-readonly CONFIG_LINE="include user.conf"
+readonly config_dir="${HOME}/.config/kitty"
+readonly config_file="${config_dir}/kitty.conf"
+readonly config_expr="include user.conf"
 
-if [ ! -d "${CONFIG_DIR}" ]; then
-  mkdir -p "${CONFIG_DIR}"
+if [ ! -d "${config_dir}" ]; then
+  mkdir -p "${config_dir}"
 fi
 
-if [ ! -e "${CONFIG_FILE}" ]; then
-  touch "${CONFIG_FILE}"
+if [ ! -e "${config_file}" ]; then
+  touch "${config_file}"
 fi
 
-if ! grep -xF "${CONFIG_LINE}" "${CONFIG_FILE}" >/dev/null 2>&1; then
-  echo "${CONFIG_LINE}" >>"${CONFIG_FILE}"
+if ! grep -xF "${config_expr}" "${config_file}" >/dev/null 2>&1; then
+  echo "${config_expr}" >>"${config_file}"
 fi
 
 os_name="$(uname)"
@@ -46,7 +48,6 @@ if [ "${os_name}" = "Darwin" ]; then
   echo "Creating the Darwin-specific kitty binary links"
   ln -sf /Applications/kitty.app/Contents/MacOS/kitty /Applications/kitty.app/Contents/MacOS/kitten ~/.local/bin/
 
-  # TODO: Maybe modify this so that the daemon is not reloaded every time.
   echo "Creating and starting the Darwin-specific kitty daemon"
   launch_agent="fi.anttikivi.kittycolors.plist"
 
