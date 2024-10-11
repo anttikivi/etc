7de7cc4142d5a20067f4ea578c84a9c3b0177782
Antti Kivi <antti@anttikivi.fi>
2024-10-11 15:06:24 +0300

    fix: fix the `nvm` script when it's not installed

--- installs/node.sh
+++ installs/node.sh
@@ -13,14 +13,20 @@ not_supported() {
 if [ "${HAS_CONNECTION}" = "true" ]; then
   minor_ver="$(echo "${NVM_VERSION}" | head -c "$(echo "${NVM_VERSION}" | grep -m 2 -ob "\." | tail -1 | grep -oE "[0-9]+")")"
   wanted_ver="$(curl -LsSH "X-GitHub-Api-Version: 2022-11-28" 'https://api.github.com/repos/nvm-sh/nvm/releases?per_page=100' | jq -r '.[] | .tag_name' | grep "${minor_ver}" | sort -V | tail -1 | cut -c 2-)"
-  current_ver="$(nvm --version)"
+  current_ver=""
+  if command -v nvm >/dev/null 2>&1; then
+    current_ver="$(nvm --version)"
+  fi
 
   install_nvm() {
     echo "Installing nvm ${wanted_ver}"
     PROFILE=/dev/null bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${wanted_ver}/install.sh | bash"
+    NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
   }
 
   if ! command -v nvm >/dev/null 2>&1; then
+    printf "%bnvm was not found%b\n" "${ESC_YELLOW}" "${ESC_RESET}"
     install_nvm
   elif [ "${wanted_ver}" != "${current_ver}" ]; then
     if [ "${DO_UPDATES}" = "true" ]; then
