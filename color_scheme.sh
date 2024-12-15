0487f6c651531f0ac2adb6dee10b0a4242ecfbc3
Antti Kivi <antti@anttikivi.fi>
2024-12-15 13:00:17 +0200

    feat: change to Tokyo Night and update plugins

--- color_scheme.sh
+++ color_scheme.sh
@@ -36,7 +36,7 @@ capitalize() {
   echo "$(echo "$1" | cut -c -1 | tr '[:lower:]' '[:upper:]')$(echo "$1" | cut -c 2-)"
 }
 
-export COLOR_SCHEME="${CATPPUCCIN}"
+export COLOR_SCHEME="${TOKYONIGHT}"
 
 case "${COLOR_SCHEME}" in
   "${CATPPUCCIN}")
