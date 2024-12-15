3f76b660149906c7aece3e05fa375aa67eafd61f
Antti Kivi <antti@anttikivi.fi>
2024-12-15 14:41:57 +0200

    feat(nvim): add keymaps for moving lines and adding comments above and below the current line

--- nvim/lua/anttikivi/config/keymaps.lua
+++ nvim/lua/anttikivi/config/keymaps.lua
@@ -15,6 +15,131 @@ vim.keymap.set(
 -- To normal mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!"<CR>')
+
+-- Make up and down take line wrapping into account
+vim.keymap.set(
+  { "n", "x" },
+  "j",
+  "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, silent = true }
+)
+vim.keymap.set(
+  { "n", "x" },
+  "k",
+  "v:count == 0 ? 'gk' : 'k'",
+  { expr = true, silent = true }
+)
+
+-- Keybinds to make split navigation easier.
+-- vim.keymap.set(
+--   "n",
+--   "<C-h>",
+--   "<C-w><C-h>",
+--   { desc = "Move focus to the left window" }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<C-l>",
+--   "<C-w><C-l>",
+--   { desc = "Move focus to the right window" }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<C-j>",
+--   "<C-w><C-j>",
+--   { desc = "Move focus to the lower window" }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<C-k>",
+--   "<C-w><C-k>",
+--   { desc = "Move focus to the upper window" }
+-- )
+
+-- Resize window using <ctrl> + arrow keys
+-- vim.keymap.set(
+--   "n",
+--   "<C-Up>",
+--   "<cmd>resize +2<cr>",
+--   { desc = "Increase Window Height" }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<C-Down>",
+--   "<cmd>resize -2<cr>",
+--   { desc = "Decrease Window Height" }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<C-Left>",
+--   "<cmd>vertical resize -2<cr>",
+--   { desc = "Decrease Window Width" }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<C-Right>",
+--   "<cmd>vertical resize +2<cr>",
+--   { desc = "Increase Window Width" }
+-- )
+
+-- Move lines
+-- TODO: I don't really like the fact that I use the arrow keys for these, but
+-- I'm running out of keys to use. Maybe I'll find a better combination later.
+vim.keymap.set(
+  "n",
+  "<A-down>",
+  "<cmd>execute 'move .+' . v:count1<cr>==",
+  { desc = "Move down" }
+)
+vim.keymap.set(
+  "n",
+  "<A-up>",
+  "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==",
+  { desc = "Move up" }
+)
+vim.keymap.set(
+  "i",
+  "<A-down>",
+  "<esc><cmd>m .+1<cr>==gi",
+  { desc = "Move down" }
+)
+vim.keymap.set("i", "<A-up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set(
+  "v",
+  "<A-down>",
+  ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv",
+  { desc = "Move Down" }
+)
+vim.keymap.set(
+  "v",
+  "<A-up>",
+  ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv",
+  { desc = "Move Up" }
+)
+
+-- Better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Commenting
+vim.keymap.set(
+  "n",
+  "gco",
+  "o<esc>vcx<esc><cmd>normal gcc<cr>fxa<bs>",
+  { desc = "Add comment below" }
+)
+vim.keymap.set(
+  "n",
+  "gcO",
+  "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>",
+  { desc = "Add comment above" }
+)
+
 -- Diagnostic keymaps
 vim.keymap.set(
   "n",
@@ -50,62 +175,12 @@ vim.keymap.set(
   { desc = "Escape and clear hlsearch" }
 )
 
--- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!"<CR>')
-
--- Keybinds to make split navigation easier.
--- vim.keymap.set(
---   "n",
---   "<C-h>",
---   "<C-w><C-h>",
---   { desc = "Move focus to the left window" }
--- )
--- vim.keymap.set(
---   "n",
---   "<C-l>",
---   "<C-w><C-l>",
---   { desc = "Move focus to the right window" }
--- )
--- vim.keymap.set(
---   "n",
---   "<C-j>",
---   "<C-w><C-j>",
---   { desc = "Move focus to the lower window" }
--- )
--- vim.keymap.set(
---   "n",
---   "<C-k>",
---   "<C-w><C-k>",
---   { desc = "Move focus to the upper window" }
--- )
-
 vim.keymap.set(
   "t",
   "<Esc><Esc>",
   "<C-\\><C-n>",
   { desc = "Exit terminal mode" }
 )
-
--- Make up and down take line wrapping into account
-vim.keymap.set(
-  { "n", "x" },
-  "j",
-  "v:count == 0 ? 'gj' : 'j'",
-  { expr = true, silent = true }
-)
-vim.keymap.set(
-  { "n", "x" },
-  "k",
-  "v:count == 0 ? 'gk' : 'k'",
-  { expr = true, silent = true }
-)
-
--- Better indenting
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
 
 -- Use the sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
