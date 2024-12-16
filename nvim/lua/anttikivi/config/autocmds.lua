b1bcdd42eb4e4379533f440f8cb01a93686c7e10
Antti Kivi <antti@anttikivi.fi>
2024-12-16 22:08:03 +0200

    feat(nvim): add the autocommands

---
+++ nvim/lua/anttikivi/config/autocmds.lua
@@ -0,0 +1,47 @@
+-- This module is based on LazyVim/LazyVim, licensed under Apache-2.0.
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("anttikivi_" .. name, { clear = true })
+end
+
+-- Highlight on yank.
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+-- Go to the last location when opening a buffer.
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_location"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if
+      vim.tbl_contains(exclude, vim.bo[buf].filetype)
+      or vim.b[buf].ak_last_loc
+    then
+      return
+    end
+    vim.b[buf].ak_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Automatically create directories when saving a file, in case some
+-- intermediate directory does not exist.
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
