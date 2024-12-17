6b143191867f9bec5d5da76ef5049222df7ee6a9
Antti Kivi <antti@anttikivi.fi>
2024-12-17 08:10:16 +0200

    feat(nvim): add Go

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -35,6 +35,7 @@ return {
     opts = {
       formatters_by_ft = {
         astro = { "prettier" },
+        go = { "goimports", "gofumpt" },
       },
     },
   },
@@ -43,6 +44,8 @@ return {
     opts = {
       ensure_installed = {
         "ansible-lint",
+        "gofumpt",
+        "goimports",
       },
     },
   },
@@ -95,6 +98,50 @@ return {
             clangdFileStatus = true,
           },
         },
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = {
+                "-.git",
+                "-.vscode",
+                "-.idea",
+                "-.vscode-test",
+                "-node_modules",
+              },
+              semanticTokens = true,
+            },
+          },
+        },
       },
       setup = {
         clangd = function(_, opts)
@@ -107,6 +154,27 @@ return {
             )
           )
           return false
+        end,
+        gopls = function()
+          -- workaround for gopls not supporting semanticTokensProvider
+          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          AK.lsp.on_attach(function(client, _)
+            if not client.server_capabilities.semanticTokensProvider then
+              local semantic =
+                client.config.capabilities.textDocument.semanticTokens
+              client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                  ---@diagnostic disable-next-line: need-check-nil
+                  tokenTypes = semantic.tokenTypes,
+                  ---@diagnostic disable-next-line: need-check-nil
+                  tokenModifiers = semantic.tokenModifiers,
+                },
+                range = true,
+              }
+            end
+          end, "gopls")
+          -- end workaround
         end,
       },
     },
