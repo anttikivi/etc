18cd46051f0b848adc5f51bd673b85300c1a3de0
Antti Kivi <antti@anttikivi.fi>
2024-12-17 08:13:41 +0200

    refactor(nvim): remove the language tool examples

--- nvim/lua/anttikivi/plugins/formatting.lua
+++ nvim/lua/anttikivi/plugins/formatting.lua
@@ -81,7 +81,6 @@ return {
         },
         formatters_by_ft = {
           lua = { "stylua" },
-          fish = { "fish_indent" },
           sh = { "shfmt" },
         },
         -- The options you set here will be merged with the builtin formatters.
@@ -89,17 +88,6 @@ return {
         ---@type table<string, conform.FormatterConfigOverride | fun(bufnr: integer): nil | conform.FormatterConfigOverride>
         formatters = {
           injected = { options = { ignore_errors = true } },
-          -- # Example of using dprint only when a dprint.json file is present
-          -- dprint = {
-          --   condition = function(ctx)
-          --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-          --   end,
-          -- },
-          --
-          -- # Example of using shfmt with extra args
-          -- shfmt = {
-          --   prepend_args = { "-i", "2", "-ci" },
-          -- },
         },
       }
       return opts
--- nvim/lua/anttikivi/plugins/lsp/init.lua
+++ nvim/lua/anttikivi/plugins/lsp/init.lua
@@ -109,11 +109,6 @@ return {
         ---@type lspconfig.options
         servers = {
           lua_ls = {
-            -- mason = false, -- set to false if you don't want this server to be installed with mason
-            -- Use this to add any additional keymaps
-            -- for specific lsp servers
-            -- ---@type LazyKeysSpec[]
-            -- keys = {},
             settings = {
               Lua = {
                 workspace = {
@@ -140,18 +135,8 @@ return {
             },
           },
         },
-        -- you can do any additional lsp server setup here
-        -- return true if you don't want this server to be setup with lspconfig
         ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-        setup = {
-          -- example to setup with typescript.nvim
-          -- tsserver = function(_, opts)
-          --   require("typescript").setup({ server = opts })
-          --   return true
-          -- end,
-          -- Specify * to use this function as a fallback for any server
-          -- ["*"] = function(server, opts) end,
-        },
+        setup = {},
       }
       return ret
     end,
