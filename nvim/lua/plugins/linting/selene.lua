7319545e3205b0f38d0a4dd599a8a765baaac916
Antti Kivi <antti@anttikivi.fi>
2024-12-17 21:38:57 +0200

    fix(nvim): fix Selene

--- nvim/lua/plugins/linting/selene.lua
+++ nvim/lua/plugins/linting/selene.lua
@@ -9,6 +9,20 @@ return {
       linters_by_ft = {
         lua = { "selene" },
       },
+      linters = {
+        selene = {
+          condition = function(ctx)
+            local root = AK.root.get({ normalize = true })
+            if root ~= vim.uv.cwd() then
+              return false
+            end
+            return vim.fs.find(
+              { "selene.toml" },
+              { path = root, upward = true }
+            )[1]
+          end,
+        },
+      },
     },
   },
 }
