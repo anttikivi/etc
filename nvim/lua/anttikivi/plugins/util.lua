e23b2e284442173c3cd8297d6fe7899734045659
Antti Kivi <antti@anttikivi.fi>
2024-12-17 07:43:33 +0200

    feat(nvim): add utility plugins

---
+++ nvim/lua/anttikivi/plugins/util.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = true,
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+    },
+  },
+}
