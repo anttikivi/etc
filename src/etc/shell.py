85762f6f4b22ac9dce37f40e585f5e6278d87096
Antti Kivi <antti@anttikivi.fi>
2024-12-22 01:34:39 +0200

    feat: revert the echo class

--- src/etc/shell.py
+++ src/etc/shell.py
@@ -6,87 +6,6 @@ from typing import TextIO, cast
 from etc.message_level import MessageLevel
 
 
-class _Echo:
-    """
-    Utility that handles the echoing for the shell type.
-
-    The classes are separated so that the shell type focuses on the
-    command execution and the echo type handles the information related
-    to what gets printed and how.
-    """
-
-    def __init__(
-        self,
-        dry_run: bool,
-        verbosity: MessageLevel,
-        print_commands: bool,
-        prompt: str = "+ ",
-    ):
-        self.verobosity: MessageLevel = verbosity
-        self.print_commands: bool = print_commands
-        self._dry_run: bool = dry_run
-        self._prompt: str = prompt
-
-    def __call__(self, s: str, file: TextIO | None = None):
-        if self.print_commands:
-            cmd = ["echo", "-n", s]
-            if file is not None and file == sys.stderr:
-                cmd.append(">&2")
-            self.command(cmd)
-        if file is not None:
-            print(s, file=file)
-        else:
-            print(s)
-
-    def command(
-        self,
-        command: list[str],
-        env: dict[str, str] | None = None,
-    ) -> None:
-        """
-        Echoes a command to command line.
-        """
-        file = sys.stderr
-        if self._dry_run:
-            file = sys.stdout
-        print(
-            f"{self._prompt}{self.quote_command(command=command, env=env)}",
-            file=file,
-        )
-        _ = file.flush()
-
-    def command_output(self, s: str):
-        print(s, file=sys.stderr)
-
-    def quote_command(
-        self, command: list[str], env: dict[str, str] | None = None
-    ) -> str:
-        output: list[str] = []
-        if env is not None:
-            output.extend(
-                [
-                    self.__quote_argument(f"{name}={value}")
-                    for name, value in sorted(env.items())
-                ]
-            )
-        output.extend([self.__quote_argument(s) for s in command])
-        return " ".join(output)
-
-    def test_e(self, file: str):
-        if self.print_commands:
-            self.command(["[", "-e", file, "]"])
-
-    def __quote_argument(self, s: str) -> str:
-        """
-        Gives a shell-escaped version of the argument.
-        """
-        if "\n" in s or "\033" in s:
-            return repr(s)
-        if s == ">&2":
-            return s
-        return shlex.quote(s)
-
-
 class Shell:
     """
     Shell executes the commands run by this script. Because it also
@@ -98,22 +17,24 @@ class Shell:
         self,
         dry_run: bool,
         verbosity: MessageLevel,
+        prompt: str = "+ ",
     ):
-        self._echo: _Echo = _Echo(
-            dry_run=dry_run,
-            verbosity=verbosity,
-            print_commands=verbosity <= MessageLevel.TRACE or dry_run,
-            prompt="+ ",
+        self.__dry_run = dry_run
+        self.__verobosity = verbosity
+        self.__print_commands = (
+            cast(bool, self.__verobosity <= MessageLevel.TRACE) or dry_run
         )
-        self._dry_run: bool = dry_run
-        self._print_outputs: bool = verbosity <= MessageLevel.TRACE
+        self.__print_outputs = cast(
+            bool, self.__verobosity <= MessageLevel.TRACE
+        )
+        self.__prompt = prompt
 
     def __call__(self, command: list[str], allow_output: bool | None = None):
-        if self._echo.print_commands:
-            self._echo.command(command)
-        if self._dry_run:
+        if self.__print_commands:
+            self.__echo_command(command)
+        if self.__dry_run:
             return
-        disable_output = not self._print_outputs
+        disable_output = not self.__print_outputs
         if allow_output is not None:
             disable_output = not allow_output
         try:
@@ -124,7 +45,7 @@ class Shell:
         except subprocess.CalledProcessError as e:
             print(
                 (
-                    f'Command "{self._echo.quote_command(command)}" exited '
+                    f'Command "{self.__quote_command(command)}" exited '
                     f"with code {e.returncode}"
                 ),
                 file=sys.stderr,
@@ -138,16 +59,16 @@ class Shell:
             print(
                 (
                     "Could not run command "
-                    f'"{self._echo.quote_command(command)}": {e.strerror}'
+                    f'"{self.__quote_command(command)}": {e.strerror}'
                 ),
                 file=sys.stderr,
             )
             sys.exit(1)
 
     def output(self, command: list[str]) -> str:
-        if self._echo.print_commands:
-            self._echo.command(command)
-        if self._dry_run:
+        if self.__print_commands:
+            self.__echo_command(command)
+        if self.__dry_run:
             return ""
         try:
             result = subprocess.run(
@@ -157,16 +78,16 @@ class Shell:
                 text=True,
             )
 
-            if self._print_outputs and result.stderr.strip() != "":
-                self._echo.command_output(result.stderr)
-            if self._print_outputs and result.stdout.strip() != "":
-                self._echo.command_output(result.stdout)
+            if self.__print_outputs and result.stderr.strip() != "":
+                self.__echo_output(result.stderr)
+            if self.__print_outputs and result.stdout.strip() != "":
+                self.__echo_output(result.stdout)
 
             return result.stdout
         except subprocess.CalledProcessError as e:
             print(
                 (
-                    f'Command "{self._echo.quote_command(command)}" exited with '
+                    f'Command "{self.__quote_command(command)}" exited with '
                     f"code {e.returncode}\nThe following output was captured:"
                 ),
                 file=sys.stderr,
@@ -180,12 +101,67 @@ class Shell:
             print(
                 (
                     "Could not run command "
-                    f'"{self._echo.quote_command(command)}": {e.strerror}'
+                    f'"{self.__quote_command(command)}": {e.strerror}'
                 ),
                 file=sys.stderr,
             )
             sys.exit(1)
 
-    @property
-    def echo(self) -> _Echo:
-        return self._echo
+    def echo(self, s: str, file: TextIO | None = None):
+        if self.__print_commands:
+            cmd = ["echo", "-n", s]
+            if file is not None and file == sys.stderr:
+                cmd.append(">&2")
+            self.__echo_command(cmd)
+        if file is not None:
+            print(s, file=file)
+        else:
+            print(s)
+
+    def echo_test_e(self, file: str):
+        if self.__print_commands:
+            self.__echo_command(["[", "-e", file, "]"])
+
+    def __echo_command(
+        self,
+        command: list[str],
+        env: dict[str, str] | None = None,
+    ) -> None:
+        """
+        Echoes a command to command line.
+        """
+        file = sys.stderr
+        if self.__dry_run:
+            file = sys.stdout
+        print(
+            f"{self.__prompt}{self.__quote_command(command=command, env=env)}",
+            file=file,
+        )
+        _ = file.flush()
+
+    def __echo_output(self, s: str):
+        print(s, file=sys.stderr)
+
+    def __quote_argument(self, s: str) -> str:
+        """
+        Gives a shell-escaped version of the argument.
+        """
+        if "\n" in s or "\033" in s:
+            return repr(s)
+        if s == ">&2":
+            return s
+        return shlex.quote(s)
+
+    def __quote_command(
+        self, command: list[str], env: dict[str, str] | None = None
+    ) -> str:
+        output: list[str] = []
+        if env is not None:
+            output.extend(
+                [
+                    self.__quote_argument(f"{name}={value}")
+                    for name, value in sorted(env.items())
+                ]
+            )
+        output.extend([self.__quote_argument(s) for s in command])
+        return " ".join(output)
